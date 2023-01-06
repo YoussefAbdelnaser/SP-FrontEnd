2ce12cd960b31b68e8b58d9a211839ac8d356cd2
YoussefAbdelnaser <120310026+YoussefAbdelnaser@users.noreply.github.com>
2023-01-07 00:22:03 +0200

    Create Analy.js

---
+++ src/components/Analy.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Analy.css";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  BarChart,
+  XAxis,
+  YAxis,
+  Legend,
+  CartesianGrid,
+  Bar,
+} from "recharts";
+
+const analy = () => {
+  const data = [
+    { name: "Tickets Purchased", users: 2000000000 },
+    { name: "Tickets Pending", users: 1500000000 },
+    { name: "Tickets Reserved", users: 1000000000 },
+  ];
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Tickets Analytics</h1>
+      <div className="App">
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="users"
+            isAnimationActive={false}
+            data={data}
+            cx={200}
+            cy={200}
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          <Tooltip />
+        </PieChart>
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 80,
+            bottom: 5,
+          }}
+          barSize={20}
+        >
+          <XAxis
+            dataKey="name"
+            scale="point"
+            padding={{ left: 10, right: 10 }}
+          />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="users" fill="#8884d8" background={{ fill: "#eee" }} />
+        </BarChart>
+      </div>
+    </div>
+  );
+};
+
+export default analy;
